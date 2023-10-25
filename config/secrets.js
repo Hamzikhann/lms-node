b3ec0c0653a8d133d4c9720ff02e2e74c0a1b0c3
Ahmad Murtaza <ahmad@oxibit.com>
2023-10-25 17:05:35 +0500

    update

--- config/secrets.js
+++ config/secrets.js
@@ -4,12 +4,15 @@ module.exports = {
 	host: process.env.HOST,
 	port: process.env.PORT,
 
+	frontend_URL: process.env.FRONTEND_URL,
+
 	crypto: {
 		algorithm: process.env.CRYPTO_ALGORITHM,
 		password: process.env.CRYPTO_PASSWORD
 	},
 
 	email: {
+		send: process.env.EMAIL_SEND,
 		sender: {
 			success: process.env.EMAIL_SEND_SUCCESS,
 			error: process.env.EMAIL__SEND_ERROR
