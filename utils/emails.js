d8d73894d90e76e928cbfe12fa86ece8e688e9d7
Ahmad Murtaza <ahmad@oxibit.com>
2023-10-25 17:15:15 +0500

    email updates

--- utils/emails.js
+++ utils/emails.js
@@ -6,7 +6,6 @@ const crypto = require("../utils/crypto");
 
 const baseURL = secrets.frontend_URL;
 
-const emailSend = secrets.email.send;
 const emailSenderError = secrets.email.sender.error;
 const emailSenderSuccess = secrets.email.sender.success;
 
@@ -18,35 +17,33 @@ function Email() {}
 
 Email.errorEmail = async (req, error) => {
 	try {
-		if (emailSend == 1) {
-			const data = fs.readFileSync("./templates/emailError.html", "utf8");
-			var text = data;
-			const userInfo = {
-				userId: req.userId ? crypto.decrypt(req.userId) : "NULL",
-				roleId: req.roleId ? crypto.decrypt(req.roleId) : "NULL",
-				role: req.role ? req.role : "NULL"
-			};
-			// =================== device info ====================
-			const DeviceDetector = require("device-detector-js");
-			const deviceDetector = new DeviceDetector();
-			const userAgent = req.headers && req.headers["user-agent"] ? req.headers["user-agent"] : null;
-			const deviceInfo = userAgent ? deviceDetector.parse(userAgent) : null;
-			//=====================================================
-			text = text.replace("[USER_INFO]", JSON.stringify(userInfo));
-			text = text.replace("[DEVICE_INFO]", JSON.stringify(deviceInfo));
-			text = text.replace("[API]", JSON.stringify(req.originalUrl));
-			text = text.replace("[METHOD]", req.method ? req.method : null);
-			text = text.replace("[REQ_BODY]", JSON.stringify(req.body));
-			text = text.replace("[REQ_PARAMS]", JSON.stringify(req.params));
-			text = text.replace("[ERROR]", error);
-			var mailOptions = {
-				from: `LMS <${emailSenderError}>`,
-				to: emailSenderError,
-				subject: "ERROR in LMS(" + req.headers.origin + ")",
-				html: text
-			};
-			return nodeMailer(mailOptions);
-		}
+		const data = fs.readFileSync("./templates/emailError.html", "utf8");
+		var text = data;
+		const userInfo = {
+			userId: req.userId ? crypto.decrypt(req.userId) : "NULL",
+			roleId: req.roleId ? crypto.decrypt(req.roleId) : "NULL",
+			role: req.role ? req.role : "NULL"
+		};
+		// =================== device info ====================
+		const DeviceDetector = require("device-detector-js");
+		const deviceDetector = new DeviceDetector();
+		const userAgent = req.headers && req.headers["user-agent"] ? req.headers["user-agent"] : null;
+		const deviceInfo = userAgent ? deviceDetector.parse(userAgent) : null;
+		//=====================================================
+		text = text.replace("[USER_INFO]", JSON.stringify(userInfo));
+		text = text.replace("[DEVICE_INFO]", JSON.stringify(deviceInfo));
+		text = text.replace("[API]", JSON.stringify(req.originalUrl));
+		text = text.replace("[METHOD]", req.method ? req.method : null);
+		text = text.replace("[REQ_BODY]", JSON.stringify(req.body));
+		text = text.replace("[REQ_PARAMS]", JSON.stringify(req.params));
+		text = text.replace("[ERROR]", error);
+		var mailOptions = {
+			from: `LMS <${emailSenderError}>`,
+			to: emailSenderError,
+			subject: "ERROR in LMS(" + req.headers.origin + ")",
+			html: text
+		};
+		return nodeMailer(mailOptions);
 	} catch (error) {
 		console.log(error);
 		throw error;
@@ -55,23 +52,21 @@ Email.errorEmail = async (req, error) => {
 
 Email.addUser = async (user) => {
 	try {
-		if (emailSend == 1) {
-			const data = fs.readFileSync("./templates/emailAddUser.html", "utf8");
-			var text = data;
+		const data = fs.readFileSync("./templates/emailAddUser.html", "utf8");
+		var text = data;
 
-			text = text.replace("[USER_NAME]", user.firstName + " " + user.lastName);
-			text = text.replace("[PASSWORD]", user.password);
-			text = text.replace("[SIGNIN_BUTTON]", process.env.frontend_URL);
+		text = text.replace("[USER_NAME]", user.firstName + " " + user.lastName);
+		text = text.replace("[PASSWORD]", user.password);
+		text = text.replace("[SIGNIN_BUTTON]", process.env.frontend_URL);
 
-			var mailOptions = {
-				from: `LMS <${emailSenderSuccess}>`,
-				to: user.email,
-				subject: "Welcome To Learning Memangement System",
-				html: text
-			};
+		var mailOptions = {
+			from: `LMS <${emailSenderSuccess}>`,
+			to: user.email,
+			subject: "Welcome To Learning Memangement System",
+			html: text
+		};
 
-			return nodeMailer(mailOptions);
-		}
+		return nodeMailer(mailOptions);
 	} catch (error) {
 		console.log(error);
 		throw error;
@@ -80,30 +75,28 @@ Email.addUser = async (user) => {
 
 Email.forgotPassword = async (user) => {
 	try {
-		if (emailSend == 1) {
-			const forgetPasswordToken = jwt.signToken({
-				userId: user.id,
-				roleId: user.roleId,
-				email: user.email
-			});
+		const forgetPasswordToken = jwt.signToken({
+			userId: user.id,
+			roleId: user.roleId,
+			email: user.email
+		});
 
-			var link = baseURL + "reset/password/" + forgetPasswordToken;
+		var link = baseURL + "reset/password/" + forgetPasswordToken;
 
-			const data = fs.readFileSync("./templates/emailForgotPassword.html", "utf8");
-			var text = data;
-			text = text.replace("[USER_NAME]", user.firstName + " " + user.lastName);
-			text = text.replace("[BUTTON_LINK_1]", link);
-			text = text.replace("[TEXT_LINK]", link);
+		const data = fs.readFileSync("./templates/emailForgotPassword.html", "utf8");
+		var text = data;
+		text = text.replace("[USER_NAME]", user.firstName + " " + user.lastName);
+		text = text.replace("[BUTTON_LINK_1]", link);
+		text = text.replace("[TEXT_LINK]", link);
 
-			var mailOptions = {
-				from: `LMS <${emailSenderSuccess}>`,
-				to: user.email,
-				subject: "Reset Password",
-				html: text
-			};
+		var mailOptions = {
+			from: `LMS <${emailSenderSuccess}>`,
+			to: user.email,
+			subject: "Reset Password",
+			html: text
+		};
 
-			nodeMailer(mailOptions);
-		}
+		nodeMailer(mailOptions);
 	} catch (error) {
 		console.log(error);
 		throw error;
--- utils/nodeMailer.js
+++ utils/nodeMailer.js
@@ -1,28 +1,31 @@
 const nodemailer = require("nodemailer");
 const secrets = require("../config/secrets");
 
+const emailSend = secrets.email.send;
+
 async function nodeMailer(mailOptions) {
-	// return 1;
+	if (emailSend == 1) {
+		const transporter = await nodemailer.createTransport({
+			host: "smtp.sendgrid.net",
+			port: 465,
+			auth: {
+				user: "apikey",
+				pass: process.env.SENDGRID_API_KEY
+			}
+		});
 
-	const transporter = await nodemailer.createTransport({
-		host: "smtp.sendgrid.net",
-		port: 465,
-		auth: {
-			user: "apikey",
-			pass: process.env.SENDGRID_API_KEY
+		try {
+			await transporter.verify();
+		} catch (error) {
+			throw error;
 		}
-	});
 
-	try {
-		await transporter.verify();
-	} catch (error) {
-		throw error;
+		const info = await transporter.sendMail(mailOptions);
+		console.log("Email sent to ", mailOptions.to, info);
+		return info;
+	} else {
+		return 1;
 	}
-
-	const info = await transporter.sendMail(mailOptions);
-	console.log(info);
-	console.log("Email sent to ", mailOptions.to);
-	return info;
 }
 
 module.exports = nodeMailer;
