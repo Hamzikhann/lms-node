5a5c5c7b91e88d368ccc7ebc6f507c749438f25e
Ahmad Murtaza <ahmad@oxibit.com>
2023-11-02 12:29:32 +0500

    course add api fix

--- modules/courses/course.controller.js
+++ modules/courses/course.controller.js
@@ -258,7 +258,7 @@ exports.create = async (req, res) => {
 					.then(async (result) => {
 						const courseId = result.id;
 
-						const courseObjectivesArr = req.body.objectives;
+						var courseObjectivesArr = req.body.objectives;
 						if (courseObjectivesArr) {
 							courseObjectivesArr = JSON.parse(courseObjectivesArr);
 							courseObjectivesArr.forEach((objective) => {
@@ -268,13 +268,13 @@ exports.create = async (req, res) => {
 								await courseObjectives.bulkInsert(courseObjectivesArr, { transaction });
 						}
 
-						const syllabus = {
+						var syllabus = {
 							title: "Table of Content",
 							courseId: courseId
 						};
 						await courseSyllabus.create(syllabus, { transaction });
 
-						const instructorObj = {
+						var instructorObj = {
 							name: req.body.instructorName,
 							about: req.body.instructorAbout,
 							courseId: courseId
